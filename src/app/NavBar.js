7e0102f2dd5133caffb8aea19827eb2f4bcbf9d1
Girijajethitech <girijashankarmohanta11@gmail.com>
2024-08-24 01:43:17 +0530

    certificate added and navbar icon added

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -3,6 +3,13 @@ import { useState } from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import '../../styles/NavBar.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from '@fortawesome/free-solid-svg-icons';   
+import { faHome } from '@fortawesome/free-solid-svg-icons';   
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'; 
+import { faProjectDiagram } from '@fortawesome/free-solid-svg-icons'; 
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; 
+
 
 export default function NavBar() {
   const pathname = usePathname();
@@ -17,6 +24,7 @@ export default function NavBar() {
       <div className="logo_container">
         <div className="logo">
           <Link href="/" className="logo_text">
+          <FontAwesomeIcon icon={faUser} className='nav_icon'/>
             Girija Shankar
           </Link>
         </div>
@@ -31,21 +39,25 @@ export default function NavBar() {
       <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
         <li>
           <Link href="/" className={pathname === '/' ? 'active' : ''}>
+          <FontAwesomeIcon icon={faHome}  className='nav_icon'/>
             Home
           </Link>
         </li>
         <li>
           <Link href="/about" className={pathname === '/about' ? 'active' : ''}>
+          <FontAwesomeIcon icon={faInfoCircle} className='nav_icon'/>
             About
           </Link>
         </li>
         <li>
           <Link href="/projects" className={pathname === '/projects' ? 'active' : ''}>
+          <FontAwesomeIcon icon={faProjectDiagram} className='nav_icon'/>
             Projects
           </Link>
         </li>
         <li>
           <Link href="/contact" className={pathname === '/contact' ? 'active' : ''}>
+          <FontAwesomeIcon icon={faEnvelope} className='nav_icon'/>
             Contact
           </Link>
         </li>
--- src/app/certificate/page.js
+++ src/app/certificate/page.js
@@ -1,21 +1,130 @@
 import "../../../styles/Certificate.css";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faFacebook, faTwitter, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import certificate from "../../../public/images/certificate-1.png";
+import certificate2 from "../../../public/images/certificate-2.png";
+import certificate3 from "../../../public/images/certificate-3.png";
+import certificate4 from "../../../public/images/certificate-4.png";
+import certificate5 from "../../../public/images/certificate-5.png";
+import certificate6 from "../../../public/images/certificate-6.png";
+
 
 export default function CertificatePage() {
-    return (
-        <div className="Certificate_container">
-            <h1>Certificate Me</h1>
-            <p>This is the Certificate page of my portfolio.</p>
-            <div className="card">
-                <img src="https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg" alt="Certificate" className="card-image" />
-                <div className="card-overlay">
-                    <h2 className="card-title">Certificate Title</h2>
-                    <div className="card-icons">
-                        <i className="fa fa-certificate"></i>
-                    </div>
-                </div>
+  return (
+    <div className="Certificate_container">
+      <h1 className="cerficate_heading">
+        Professi<span>onal Cou</span>rses and <span>Certi</span>fications
+      </h1>
+      <p className="certificate_para">
+        “Here are the <span>certifications</span> and <span>courses</span> I’ve
+        completed, showcasing my commitment to <span>professional</span>{" "}
+        development and <span>continuous</span> learning.”
+      </p>
+      <div className="certificate_cards">
+        <div className="card">
+          <Image src={certificate} alt="certificate" className="card-image" />
+          <div className="card-overlay">
+            <h1>Coursera</h1>
+            <h4 className="card-title">Big Data Analysis with SQL</h4>
+            <div className="card-icons">
+              <a
+                href="https://www.coursera.org/account/accomplishments/certificate/E8VBAA2MQFKV"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faGlobe} />
+                <span>View Certificate</span>
+              </a>
             </div>
+          </div>
         </div>
-    );
+        <div className="card">
+          <Image src={certificate2} alt="certificate" className="card-image" />
+          <div className="card-overlay">
+            <h1>Google Cloud - Minnesota</h1>
+            <h4 className="card-title">Google Cloud Product Fundamentals em Português Brasileiro</h4>
+            <div className="card-icons">
+              <a
+                href="https://www.coursera.org/account/accomplishments/certificate/47VF2Y2KE6V5"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faGlobe} />
+                <span>View Certificate</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <Image src={certificate3} alt="certificate" className="card-image" />
+          <div className="card-overlay">
+            <h1>The Hong Kong University of Science and Technology</h1>
+            <h4 className="card-title">Front-End Web Development with React</h4>
+            <div className="card-icons">
+              <a
+                href="https://www.coursera.org/account/accomplishments/certificate/F5TWBGCL6RLQ"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faGlobe} />
+                <span>View Certificate</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <Image src={certificate4} alt="certificate" className="card-image" />
+          <div className="card-overlay">
+            <h1>Duke University</h1>
+            <h4 className="card-title">Programming Foundations with JavaScript, HTML and CSS</h4>
+            <div className="card-icons">
+              <a
+                href="https://www.coursera.org/account/accomplishments/certificate/EB2ZW6QYGV6W"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faGlobe} />
+                <span>View Certificate</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <Image src={certificate5} alt="certificate" className="card-image" />
+          <div className="card-overlay">
+            <h1>The Hong Kong University of Science and Technology</h1>
+            <h4 className="card-title">Front-End Web UI Frameworks and Tools: Bootstrap 4</h4>
+            <div className="card-icons">
+              <a
+                href="https://www.coursera.org/account/accomplishments/certificate/D67DAQW54XHN"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faGlobe} />
+                <span>View Certificate</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <Image src={certificate5} alt="certificate" className="card-image" />
+          <div className="card-overlay">
+            <h1>freeCodeCamp</h1>
+            <h4 className="card-title">Responsive Web Design</h4>
+            <div className="card-icons">
+              <a
+                href="https://www.freecodecamp.org/certification/GirijaShankar/responsive-web-design"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faGlobe} />
+                <span>View Certificate</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
